eb3b41ae8b02bd8ad6e1e4bd62e856f117fffbdb
Chapa03 <chapaog@gmail.com>
2022-02-07 19:16:17 +0100

    Primer volcado del pap con SpringBoot y Java

---
+++ papSpring/src/main/java/org/chapa/papSpring/controller/HomeController.java
@@ -0,0 +1,10 @@
+package org.chapa.papSpring.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+public class HomeController {
+	@GetMapping("/")
+	public String index() {
+		return "home/index.html";
+	}
+}
---
+++ papSpring/src/test/java/org/chapa/papSpring/PapSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package org.chapa.papSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PapSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
